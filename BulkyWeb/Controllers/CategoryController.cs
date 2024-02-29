8aa0bd0a4d30c6f0f003d4d1ab467fb5f9956b90
returnaction <obergannv@gmail.com>
2024-02-29 15:23:24 -0500

    Crud for Create is completed

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -20,6 +20,74 @@ namespace BulkyWeb.Controllers
             return View(categories);
         }
 
-        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+
+            TempData["success"] = "Category created successfuly";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Category? category = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (category is null)
+                return NotFound();
+
+            return View(category);
+
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteAction(int id)
+        {
+            Category? category = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+            _context.Categories.Remove(category!);
+            _context.SaveChanges();
+
+            TempData["success"] = "Category was deleted successfully";
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Category? category = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (category is null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+
+            TempData["success"] = "Category was updated succussfully";
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
---
+++ BulkyWeb/Migrations/20240229194305_DisplayOrderRequired.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class DisplayOrderRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DisplayOrder",
+                table: "Categories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DisplayOrder",
+                table: "Categories",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}

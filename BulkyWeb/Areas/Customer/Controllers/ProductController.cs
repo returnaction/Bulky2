6ba8ba72af17db6f25e7ae61f1625d7b446a2131
returnaction <obergannv@gmail.com>
2024-02-29 21:34:11 -0500

    Products Index is done

---
+++ BulkyWeb/Areas/Customer/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using BulkyWeb.Models;
+using BulkyWeb.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BulkyWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll();
+            return View(products);
+        }
+    }
+}
